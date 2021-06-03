bcac1da04de14419ff989637951a4c008a520135
Dmytro Kotliar <dn270571kdb@gmail.com>
2021-06-03 04:02:31 +0300

    my version

--- src/homework02/Homework02.java
+++ src/homework02/Homework02.java
@@ -1,9 +1,87 @@
 package homework02;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+import java.io.UnsupportedEncodingException;
+//import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class Homework02 {
 
-    public static void main(String[] args) {
+    private static final String ENCODING_UTF8 = "UTF-8";
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
         
+        String name = "";
+        int level;
+        
+        //try(Scanner in = new Scanner(new FileReader("src\\file.txt"))){
+        //    name = in.nextLine();
+        try(DataInputStream dis = new DataInputStream(new FileInputStream("src\\Pudge.bin"))){
+            name = dis.readUTF();
+        }catch (FileNotFoundException e){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите имя: ");
+            name = in.nextLine();
+        }catch (IOException e){
+
+        }
+        System.out.println("имя: " + name);
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите уровень: ");
+        while (true){
+            if (in.hasNextInt()){
+                level = in.nextInt();
+                System.out.println("уровень: " + level);
+                break;
+            }else{
+                in.next();
+            }
+        }
+        in.close();
+        
+        Locale locale = new Locale(args[0], args[1]);
+        ResourceBundle rb = ResourceBundle.getBundle("text", locale);
+
+        //System.out.println(rb.keySet());
+
+        Set<String> phrasesSet = new HashSet<String>();
+        int phraseLevel;
+        for (String key : rb.keySet()) {
+            phraseLevel = Integer.parseInt(key.substring(5,key.indexOf(".")));
+            String value = new String(rb.getString(key).getBytes(StandardCharsets.ISO_8859_1), ENCODING_UTF8);
+            //System.out.println(value);
+            if(phraseLevel==level){
+                phrasesSet.add(value);
+            }    
+        }
+
+        String[] phrases = phrasesSet.toArray(new String[phrasesSet.size()]);
+        System.out.println(Arrays.asList(phrases));
+        
+        Pudge p = new Pudge(name, level, phrases);
+        
+        try(DataOutputStream dos = new DataOutputStream(new FileOutputStream("src\\Pudge.bin"))){
+            dos.writeUTF(p.getName());
+            dos.writeInt(p.getLevel());
+            for (int i=0; i<phrases.length; i++){
+                dos.writeUTF(phrases[i]);
+            }
+            System.out.println("Success");
+        }catch(IOException ex){
+            
+        }
     }
 
 }
--- src/homework02/Pudge.java
+++ src/homework02/Pudge.java
@@ -2,4 +2,38 @@ package homework02;
 
 public class Pudge {
 
+    String name;
+    int level;
+    String phrases[];
+    
+    public Pudge(String name, int level, String phrases[]){
+        this.name = name;
+        this.level = level;
+        this.phrases = phrases;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String[] getPhrases() {
+        return phrases;
+    }
+
+    public void setPhrases(String[] phrases) {
+        this.phrases = phrases;
+    }
+    
 }
